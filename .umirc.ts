f50c97f4e65d858f0d55a755b8f8dc8079c7758d
benjun.xu(徐本俊) <benjun.xu@transsion.com>
2021-12-13 20:28:32 +0800

    Add ssr

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'umi';
 
+const outputPath = 'dist/';
+const env = process.env.NODE_ENV;
+const path = env === 'development' ? 'http://127.0.0.1:8000/' : outputPath;
+
 export default defineConfig({
+  mock: false,
+  ssr: {
+    // 开发模式下的服务端渲染，默认为 true
+    devServerRender: false,
+  },
   title: '这是个商城',
   nodeModulesTransform: {
     type: 'none',
   },
+  outputPath: outputPath,
+  publicPath: path,
   // history: { type: 'hash' },
   // base: '/kkb',
   theme: {
